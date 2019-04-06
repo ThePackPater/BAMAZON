e6f2754ab00126cc712434ef08d0643281d002a3
ThePackPater <thepackpater@gmail.com>
2019-04-06 09:04:03 -0500

    inquirer update

--- Bamazon.sql
+++ Bamazon.sql
@@ -15,32 +15,16 @@ CREATE TABLE Bamazon(
 );
 
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "LongBow", 100, 275, 0, 220, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "BroadSword", 100, 500, 0, 400, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "PoleAxe", 100, 200, 0, 160, 0);
+VALUES ("Elvin Weapons", "LongBow", 100, 575, 0, 420, 0);
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
 VALUES ("Elvin Weapons", "ShortSword", 100, 300, 0, 240, 0);
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "BattleAxe", 100, 350, 0, 280, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
 VALUES ("Elvin Weapons", "Dagger", 100, 125, 0, 100, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "WarHammer", 100, 375, 0, 300, 0);
 
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "LongBow", 100, 150, 0, 120, 0);
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
 VALUES ("Dwarvin Weapons", "BroadSword", 100, 750, 0, 600, 0);
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "PoleAxe", 100, 300, 0, 240, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "ShortSword", 100, 400, 0, 320, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
 VALUES ("Dwarvin Weapons", "BattleAxe", 100, 450, 0, 360, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "Dagger", 100, 200, 0, 160, 0);
 INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
 VALUES ("Dwarvin Weapons", "WarHammer", 100, 475, 0, 380, 0);
 
--- BamazonCus.js
+++ BamazonCus.js
@@ -1,47 +1,130 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var divider = "\n------------------------------------------------------------\n\n";
+var divider = "\n------------------------------------------------------------\n";
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "password",
+    user: "root",
     password: "password",
-    database: "Bamazon.sql"
+    database: "Bamazon_db"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected");
+    bamazonStart();
 });
 
-inquirer
-    .prompt([{
-        type: "list",
-        message: "Welcome to Bamazon:MoneyZgone: What access do you require?",
-        choices: ["CustomerAccess", "ManagerAccess", "CEOAccess"],
-        name: "userRole"
-    }]).then(function(inquirerResp) {
-        var User = inquirerResp.userRole;
-        console.log("\nVerifying Bamazon access: " + User);
-        console.log("One moment please!\n");
+function bamazonStart() {
+    console.log(divider);
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "Welcome to Bamazon:MoneyZgone: What access do you require?",
+            choices: ["CustomerAccess", "ManagerAccess", "CEOAccess"],
+            name: "userRole",
+        }]).then(function(inquirerResp) {
+            var User = inquirerResp.userRole;
+            console.log("\nVerifying Bamazon access: " + User);
+            console.log("One moment please!\n");
+            switch (User) {
+                case "CustomerAccess":
+                    console.log("Customer Access Granted!");
+                    console.log(divider);
+                    allowCust();
+                    break;
+                case "ManagerAccess":
+                    console.log("Manager Access Granted!");
+                    connection.end();
+                    //allowMgr();
+                    break;
+                case "CEOAccess":
+                    console.log("CEO Access Granted!");
+                    connection.end();
+                    //allowCEO();
+                    break;
+                default:
+                    console.log("I'm sorry you cannot access Bamazon");
+                    connection.end();
+                    //tryAgain();
+                    break;
+            }
+        });
+}
+
+function allowCust() {
+    connection.query("SELECT * FROM Bamazon", function(err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt({
+                name: "purchase",
+                type: "list",
+                choices: ["dwarven weapons", "elven weapons"],
+                message: "What type of weapons would you like to purchase?"
+            })
+            .then(function(answer) {
+                if (answer.purchase === "dwarven weapons") {
+                    console.log(divider);
+                    console.log("Heaft and Damage, excellent choice if you got the gold...");
+                    inquirer
+                        .prompt({
+                            name: "dwarven",
+                            type: "list",
+                            choices: ["broadsword", "battleaxe", "warhammer"],
+                            message: "What weapon would you like to purchase?"
+                        })
+                        .then(function(answer) {
+                            console.log(divider);
+
+                            switch (answer) {
+                                case "broadsword":
+                                    console.log("Wanna cut a wide swath from your enemy ranks do ya?!");
+                                    console.log(divider);
+                                    break;
+                                case "battleaxe":
+                                    console.log("Looking to cleave a skull or two?!");
+                                    console.log(divider);
+                                    break;
+                                case "warhammer":
+                                    console.log("Oh I like your style, feel like smashing your foes?");
+                                    console.log(divider);
+                                    break;
+                            }
+                        });
+                } else if (answer.purchase === "elven weapons") {
+                    console.log(divider);
+                    console.log("Speed and Magic, excellent choice if you got the gold...");
+                    inquirer
+                        .prompt({
+                            name: "dwarven",
+                            type: "list",
+                            choices: ["longbow", "shortsword", "dagger"],
+                            message: "What weapon would you like to purchase?"
+                        })
+                        .then(function(answer) {
+                            console.log(divider);
+
+                            switch (answer) {
+                                case "longbow":
+                                    console.log("Want to get some distance between yourself and a murderous ORC?!");
+                                    console.log(divider);
+                                    break;
+                                case "shortsword":
+                                    console.log("Do you need to get up close and personal?!");
+                                    console.log(divider);
+                                    break;
+                                case "dagger":
+                                    console.log("Needing to kill someone before they know you're there?!");
+                                    console.log(divider);
+                                    break;
+                            }
+                        });
+                } else {
+                    console.log(divider);
+                    connection.end();
+                }
+            });
+        connection.end();
     });
-// switch (User) {
-//     case "CustomerAccess":
-//         allowCust();
-//         break;
-//     case "ManagerAccess":
-//         allowMgr();
-//         break;
-//     case "CEOAccess":
-//         allowCEO();
-//         break;
-//     default:
-//         console.log("I'm sorry you cannot access Bamazon");
-//         tryAgain();
-//         break;
 
-//}
-
-
-// });
+}
