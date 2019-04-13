64a9d736ff4a7c579100ac4fcf55afc8c11a7da8
ThePackPater <thepackpater@gmail.com>
2019-04-13 14:52:27 -0500

    update

--- Bamazon.sql
+++ Bamazon.sql
@@ -6,25 +6,25 @@ CREATE TABLE Bamazon(
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     Department_Name VARCHAR(100),
     Product VARCHAR(50),
-    Product_Inventory INT DEFAULT 00,
-    Product_Price INT DEFAULT 00,
-    Product_Sales INT DEFAULT 00,
-    Overhead_Cost INT DEFAULT 00,
-    Total_Profit INT DEFAULT 00,
+    Product_Inventory INT,
+    Product_Price INT,
+    Product_Sales INT,
+   
+    
     PRIMARY KEY (id)
 );
 
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "LongBow", 100, 575, 0, 420, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "ShortSword", 100, 300, 0, 240, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Elvin Weapons", "Dagger", 100, 125, 0, 100, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Elvin Weapons", "LongBow", 10, 575, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Elvin Weapons", "ShortSword", 10, 300, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Elvin Weapons", "Dagger", 10, 125, 0);
 
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "BroadSword", 100, 650, 0, 500, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "BattleAxe", 100, 450, 0, 360, 0);
-INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales, Overhead_Cost, Total_Profit)
-VALUES ("Dwarvin Weapons", "WarHammer", 100, 475, 0, 380, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Dwarvin Weapons", "BroadSword", 10, 650, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Dwarvin Weapons", "BattleAxe", 10, 450, 0);
+INSERT INTO Bamazon (Department_Name, Product, Product_Inventory, Product_Price, Product_Sales)
+VALUES ("Dwarvin Weapons", "WarHammer", 10, 475, 0);
 
--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,10 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var chalk = require('chalk');
+
 var divider = "\n------------------------------------------------------------\n";
+
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,6 +16,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+
     bamazonStart();
 });
 
@@ -22,7 +26,7 @@ function bamazonStart() {
         .prompt([{
             type: "list",
             message: chalk.magenta.bold("Welcome to Bamazon: MoneyZgone: Inhumnan Weapons Division: What access do you require?"),
-            choices: ["CustomerAccess", "ManagerAccess"],
+            choices: ["CustomerAccess", "ManagerAccess", "Exit"],
             name: "userRole",
         }]).then(function(inquirerResp) {
             var User = inquirerResp.userRole;
@@ -35,109 +39,170 @@ function bamazonStart() {
                     break;
                 case "ManagerAccess":
                     console.log(chalk.green.bold("Welcome GREAT EVIL OVERLORD!!!\n"));
-                    connection.end();
                     allowMgr();
                     break;
+                case "Exit":
+                    break;
+                    // connection.end();
+
             }
         });
 }
 
 function allowCust() {
-    connection.query("SELECT * FROM Bamazon", function(err, results) {
-        if (err) throw err;
-        inquirer
-            .prompt({
-                name: "purchase",
-                type: "list",
-                choices: ["Purchase dwarven weapons", "Purchase elven weapons", "I'm finished"],
-                message: chalk.magenta.bold("What do you need Warrior?")
-            })
-            .then(function(answer) {
-                if (answer.purchase === "Purchase dwarven weapons") {
-                    console.log(divider);
-                    console.log(chalk.red("Heft and Damage, excellent choice if you got the gold..."));
-                    console.log(divider);
-                    inquirer
-                        .prompt({
-                            name: "dwarven",
-                            type: "list",
-                            choices: ["broadsword", "battleaxe", "warhammer", "none"],
-                            message: chalk.magenta("Which weapon would you like to purchase?")
-                        })
-                        .then(function(inquirerResp) {
-                            var user = inquirerResp.dwarven;
-                            switch (user) {
-                                case "broadsword":
-                                    console.log(chalk.cyan("Wanna cut a wide swath from your enemy ranks do ya?!"));
-                                    console.log(chalk.cyan("Our Dwarven Broadswords are 650 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "battleaxe":
-                                    console.log(chalk.cyan("Looking to cleave a skull or two?!"));
-                                    console.log(chalk.cyan("Our Dwarven Battleaxes are 450 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "warhammer":
-                                    console.log(chalk.cyan("Oh I like your style, feel like smashing your foes?"));
-                                    console.log(chalk.cyan("Our Dwarven Warhammers are 475 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "none":
-                                    allowCust();
-                                    break;
-                            }
-                        });
+    inquirer
+        .prompt({
+            name: "purchase",
+            type: "list",
+            choices: ["Purchase dwarven weapons", "Purchase elven weapons", "I'm finished"],
+            message: chalk.magenta.bold("What do you need Warrior?")
+        })
+        .then(function(answer) {
+            if (answer.purchase === "Purchase dwarven weapons") {
+                console.log(divider);
+                console.log(chalk.red("Heft and Damage, excellent choice if you got the gold..."));
+                console.log(divider);
+                inquirer
+                    .prompt({
+                        name: "dwarven",
+                        type: "list",
+                        choices: ["broadsword", "battleaxe", "warhammer", "none"],
+                        message: chalk.magenta("Which weapon would you like to purchase?")
+                    })
+                    .then(function(inquirerResp) {
+                        var user = inquirerResp.dwarven;
+                        switch (user) {
+                            case "broadsword":
+                                console.log(chalk.cyan("Wanna cut a wide swath from your enemy ranks do ya?!"));
 
-                } else if (answer.purchase === "Purchase elven weapons") {
-                    console.log(chalk.red("Speed and Magic, excellent choice if you got the gold..."));
-                    console.log(divider);
-                    inquirer
-                        .prompt({
-                            name: "elven",
-                            type: "list",
-                            choices: ["longbow", "shortsword", "dagger", "none"],
-                            message: chalk.magenta("What weapon would you like to purchase?")
-                        })
-                        .then(function(inquirerResp) {
-                            var user = inquirerResp.elven;
-                            switch (user) {
-                                case "longbow":
-                                    console.log(chalk.cyan("Want to get some distance between yourself and a murderous ORC?!"));
-                                    console.log(chalk.cyan("Our Elven Longbows are 575 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "shortsword":
-                                    console.log(chalk.cyan("Do you need to get up close and personal?!"));
-                                    console.log(chalk.cyan("Our Elven Shortswords are 300 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "dagger":
-                                    console.log(chalk.cyan("Needing to kill someone before they know you're there?!"));
-                                    console.log(chalk.cyan("Our Elven daggers are 100 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
-                                    console.log(divider);
-                                    allowCust();
-                                    break;
-                                case "none":
-                                    allowCust();
-                                    break;
-                            }
-                        });
-                } else if (answer.purchase === "I'm finished") {
-                    console.log(chalk.red("Good Questing Traveller come see me when you've got more gold!"));
-                    console.log(divider);
-                    connection.end();
-                }
-            });
-    });
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "BroadSword"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " are 650 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "battleaxe":
+                                console.log(chalk.cyan("Looking to cleave a skull or two?!"));
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "BattleAxe"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " are 450 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "warhammer":
+                                console.log(chalk.cyan("Oh I like your style, feel like smashing your foes?"));
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "WarHammer"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " 475 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "none":
+                                allowCust();
+                                break;
+                        }
+                    });
 
+            } else if (answer.purchase === "Purchase elven weapons") {
+                console.log(chalk.red("Speed and Magic, excellent choice if you got the gold..."));
+                console.log(divider);
+                inquirer
+                    .prompt({
+                        name: "elven",
+                        type: "list",
+                        choices: ["longbow", "shortsword", "dagger", "none"],
+                        message: chalk.magenta("What weapon would you like to purchase?")
+                    })
+                    .then(function(inquirerResp) {
+                        var user = inquirerResp.elven;
+                        switch (user) {
+                            case "longbow":
+                                console.log(chalk.cyan("Want to get some distance between yourself and a murderous ORC?!"));
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "LongBow"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " are 575 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "shortsword":
+                                console.log(chalk.cyan("Do you need to get up close and personal?!"));
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "ShortSword"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " are 300 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "dagger":
+                                console.log(chalk.cyan("Needing to kill someone before they know you're there?!"));
+                                connection.query(
+                                    "UPDATE bamazon SET ? WHERE ?", [{
+                                        Product_Inventory: -1,
+                                        Product_Sales: +1
+                                    }, {
+                                        Product: "Dagger"
+                                    }],
+                                    function(err, res) {
+                                        console.log(chalk.green(res.affectedRows + " are 100 gold pieces, I'll add one to your your cart and send a Goblin to collect the gold. Thank You!"));
+                                        console.log(res.affectedRows + " products updated!\n");
+                                        console.log(divider);
+                                        allowCust();
+                                    });
+                                break;
+                            case "none":
+                                allowCust();
+                                break;
+                        }
+                    });
+            } else if (answer.purchase === "I'm finished") {
+                console.log(chalk.red("Good Questing Traveller come see me when you've got more gold!"));
+                console.log(divider);
+                bamazonStart();
+            }
+        });
 }
 
 function allowMgr() {
+
     inquirer
         .prompt({
             name: "audit",
@@ -151,31 +216,70 @@ function allowMgr() {
                 case "Check Inventory":
                     console.log(chalk.cyan("Pulling the inventory Master.. soon... soon..."));
                     console.log(divider);
-                    //readProduct();
-                    allowMgr();
+                    readProduct();
+                    //allowMgr();
                     break;
                 case "Update Items":
-                    console.log(chalk.cyan("Yes Master! Change them, change them and make them obey!!"));
+                    console.log(chalk.cyan("\nYes Master! Change them, change them and make them obey!!"));
                     console.log(divider);
-                    //updateProduct();
-                    allowMgr();
+                    updateProduct();
+                    //allowMgr();
                     break;
                 case "Add items":
-                    console.log(chalk.cyan("Yes Master! More inhuman death devices!!"));
+                    console.log(chalk.cyan("\nYes Master! More inhuman death devices!!"));
                     console.log(divider);
-                    //createProduct();
-                    allowMgr();
+                    createProduct();
+                    //allowMgr();
                     break;
                 case "Delete Items":
-                    console.log(chalk.cyan("Remove the shameful failures Master, SPITE THEM!!"));
+                    console.log(chalk.cyan("\nRemove the shameful failures Master, SPITE THEM!!"));
                     console.log(divider);
-                    //removeProduct();
-                    allowMgr();
+                    removeProduct();
+                    //allowMgr();
                     break;
                 case "I'm Finished":
-                    console.log(chalk.cyan("Thank you Master, Dark Blessings upon you!"));
+                    console.log(chalk.cyan("\nThank you Master, Dark Blessings upon you!"));
                     console.log(divider);
+                    bamazonStart();
                     break;
             }
         });
+
+}
+
+function readProduct() {
+
+    connection.query("SELECT * FROM bamazon", function(err, res) {
+        if (err) throw err;
+        console.log(divider);
+        console.log(" _____________________________________________");
+        console.log("     Department    " + "   Product   " + " Inv  " + " Sales");
+        console.log(" _____________________________________________");
+        console.log(" | " + res[0].Department_Name + "   | " + res[0].Product + "    | " + res[0].Product_Inventory + " |   " + res[0].Product_Sales + "  | ");
+        console.log(" | " + res[1].Department_Name + "   | " + res[1].Product + " | " + res[1].Product_Inventory + " |   " + res[1].Product_Sales + "  | ");
+        console.log(" | " + res[2].Department_Name + "   | " + res[2].Product + "     | " + res[2].Product_Inventory + " |   " + res[2].Product_Sales + "  | ");
+        console.log(" | " + res[3].Department_Name + " | " + res[3].Product + " | " + res[3].Product_Inventory + " |   " + res[3].Product_Sales + "  | ");
+        console.log(" | " + res[4].Department_Name + " | " + res[4].Product + "  | " + res[4].Product_Inventory + " |   " + res[4].Product_Sales + "  | ");
+        console.log(" | " + res[5].Department_Name + " | " + res[5].Product + "  | " + res[5].Product_Inventory + " |   " + res[5].Product_Sales + "  | ");
+        console.log(" _____________________________________________\n");
+        console.log(divider);
+        allowMgr();
+    });
+
+}
+
+function updateProduct() {
+    connection.query(
+        "UPDATE bamazon SET ? WHERE ?", [{
+
+            },
+            {
+
+            }
+        ],
+        function(err, res) {
+            console.log(res.affectedRows + " products updated!\n");
+        }
+    );
+    allowMgr();
 }
